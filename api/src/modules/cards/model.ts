fedd575cb381f274f46a38c4354ea65a57456256
Zindiks <103096207+Zindiks@users.noreply.github.com>
2025-01-14 23:48:02 +0200

    #21 | feat: Add card module with CRUD operations

---
+++ api/src/modules/cards/model.ts
@@ -0,0 +1,85 @@
+import { Type, Static } from "@sinclair/typebox"
+import exp from "constants"
+
+const id = Type.String({ format: "uuid" })
+const list_id = Type.String({ format: "uuid" })
+const title = Type.String({ minLength: 3, maxLength: 100 })
+const order = Type.Number()
+const description = Type.String({ minLength: 3, maxLength: 255 })
+const status = Type.String()
+
+const created_at = Type.String({ format: "date-time" })
+const updated_at = Type.String({ format: "date-time" })
+
+
+const CreateCardSchema = Type.Object(
+  {
+    list_id,
+    title,
+    description,
+    status,
+  },
+  { $id: "CreateCardSchema" }
+)
+const UpdateCardTitleSchema = Type.Object(
+  {
+    id,
+    list_id,
+    title,
+  },
+  { $id: "UpdateCardTitleSchema" }
+)
+
+const UpdateCardOrderSchema = Type.Object(
+  {
+    id,
+    list_id,
+    order,
+  },
+  { $id: "UpdateCardOrderSchema" }
+)
+
+const UpdateCardOrderSchemaArray = Type.Array(UpdateCardOrderSchema)
+
+const FullCardResponseSchema = Type.Object(
+  {
+    id,
+    list_id,
+    title,
+    description,
+    status,
+    order,
+    created_at,
+    updated_at,
+  },
+  { $id: "CardResponseSchema" }
+)
+
+const FullCardResponseSchemaArray = Type.Array(FullCardResponseSchema)
+
+const DeleteCardSchema = Type.Object(
+  {
+    id,
+    list_id,
+  },
+  { $id: "DeleteCardSchema" }
+)
+
+export type CreateCard = Static<typeof CreateCardSchema>
+export type UpdateCardTitle = Static<typeof UpdateCardTitleSchema>
+export type UpdateCardOrder = Static<typeof UpdateCardOrderSchema>
+export type UpdateCardOrderArray = Static<typeof UpdateCardOrderSchemaArray>
+export type FullCardResponse = Static<typeof FullCardResponseSchema>
+export type FullCardResponseArray = Static<typeof FullCardResponseSchemaArray>
+
+export type DeleteCard = Static<typeof DeleteCardSchema>
+
+export const CardSchema = {
+  CreateCardSchema,
+  UpdateCardTitleSchema,
+  UpdateCardOrderSchema,
+  UpdateCardOrderSchemaArray,
+  FullCardResponseSchema,
+  FullCardResponseSchemaArray,
+  DeleteCardSchema,
+}
